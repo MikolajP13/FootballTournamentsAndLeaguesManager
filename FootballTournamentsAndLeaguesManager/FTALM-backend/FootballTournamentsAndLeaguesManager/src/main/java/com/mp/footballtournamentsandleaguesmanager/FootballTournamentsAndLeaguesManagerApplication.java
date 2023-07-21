e4717f495acbd8b20609dcbe62f635c5e176ca0f
MikolajP13 <mikolajp1313@gmail.com>
2023-07-21 11:44:59 +0200

    Initial commit

---
+++ FootballTournamentsAndLeaguesManager/FTALM-backend/FootballTournamentsAndLeaguesManager/src/main/java/com/mp/footballtournamentsandleaguesmanager/FootballTournamentsAndLeaguesManagerApplication.java
@@ -0,0 +1,13 @@
+package com.mp.footballtournamentsandleaguesmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballTournamentsAndLeaguesManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballTournamentsAndLeaguesManagerApplication.class, args);
+    }
+
+}
---
+++ FootballTournamentsAndLeaguesManager/FTALM-backend/FootballTournamentsAndLeaguesManager/src/test/java/com/mp/footballtournamentsandleaguesmanager/FootballTournamentsAndLeaguesManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mp.footballtournamentsandleaguesmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballTournamentsAndLeaguesManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
