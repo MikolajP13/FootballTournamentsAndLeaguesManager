0d026af0c19a8adf775bbccab992820ea2ae1e3e
MikolajP13 <mikolajp1313@gmail.com>
2023-08-20 16:14:29 +0200

    Add models class

---
+++ FootballTournamentsAndLeaguesManager/FTALM-frontend/FootballTournamentsAndLeaguesManager/src/app/models/TournamentLeagueBase/tournamentLeagueBase.ts
@@ -0,0 +1,17 @@
+export class TournamentLeagueBase {
+  id?: number;
+  name?: string;
+  numberOfTeams?: number;
+  startDate?: Date;
+  status?: Status;
+  competitionType?: CompetitionType;
+  CompetitionType: any;
+}
+
+enum Status {
+  NOT_STARTED, IN_PROGRESS, FINISHED
+}
+
+export enum CompetitionType {
+  LEAGUE = "league", TOURNAMENT = "tournament"
+}
